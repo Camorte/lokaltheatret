cb26bb5ccf8c1478a0699b09f11e4bbd97d08ad1
Carlo Morte <carlo.alfredo.malca.miguel.morte@schibsted.com>
2024-09-16 08:42:49 +0200

    adjust transition on banner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,14 +20,14 @@ const Home = () => {
             {!isLoading && mainBanner && (
                 <div className="relative group hover:cursor-pointer">
                     <div className="absolute z-[4] shadow-inner-lg h-full w-full" />
-                    <div className="bg-black absolute h-full w-full z-0 opacity-0 group-hover:opacity-100 " />
+                    <div className="bg-black absolute h-full w-full z-0 opacity-100" />
                     <img
-                        className="w-full object-cover h-[50vh] md:h-full max-h-[70vh] group-hover:opacity-[0.8]"
+                        className="relative z-[2] w-full object-cover h-[50vh] md:h-full max-h-[70vh] ease-in transition-opacity duration-300 opacity-100 group-hover:opacity-[0.8]"
                         src={urlFor(mainBanner.image).url()}
                         alt={mainBanner.caption}
                     />
                     <img
-                        className="absolute bottom-10 right-0 max-w-[250px] md:max-w-[500px] ease-in duration-300 group-hover:scale-110"
+                        className="absolute bottom-10 right-0 max-w-[250px] md:max-w-[500px] ease-in duration-300 z-[3] group-hover:scale-110"
                         src={urlFor(mainBanner.logo).url()}
                         alt={mainBanner.logoCaption}
                     />
