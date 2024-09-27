501d80f228910d34c92332f57338a2447da1b51c
Carlo Morte <carlo.alfredo.malca.miguel.morte@schibsted.com>
2024-09-28 00:40:30 +0200

    add footer and updaate layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from 'react';
 import Header from './Header.tsx';
+import Footer from './Footer.tsx';
 
 const Layout = ({ children }: { children?: ReactNode }) => {
     return (
         <div>
             <Header />
             {children}
+            <Footer />
         </div>
     );
 };
