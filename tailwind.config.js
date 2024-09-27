091a4a10437f6108f4da3b1374a04fcd5aad4911
Carlo Morte <carlo.alfredo.malca.miguel.morte@schibsted.com>
2024-09-28 00:38:46 +0200

    add will change property

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {
+            willChange: {
+                'transform-opacity': 'transform, opacity'
+            },
             keyframes: {
                 slideIn: {
                     '0%': { transform: 'translateX(100%)' },
