e413c5fec3a42fd227378b965f99dba9f893b56c
Carlo Morte <carlo.alfredo.malca.miguel.morte@schibsted.com>
2024-10-05 12:46:54 +0200

    add new image modal and spinner components

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -2,49 +2,61 @@ import { SanityImage } from '../lib/types.ts';
 import { urlFor } from '../lib/sanity.ts';
 import { IoIosArrowForward } from 'react-icons/io';
 import { IoIosArrowBack } from 'react-icons/io';
-import { MutableRefObject, useRef } from 'react';
+import { MutableRefObject, useRef, useState } from 'react';
+import ImageModal from './ImageModal.tsx';
 
 const ImageGallery = ({ images }: { images: SanityImage[] }) => {
     const scrollToRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
+    const [chosenImg, setChosenImg] = useState<SanityImage | undefined>(
+        undefined
+    );
 
     const handleScroll = (scrollLeft: boolean) => {
         const currScrollRef = scrollToRef.current;
         if (currScrollRef) {
             currScrollRef.scrollLeft +=
-                currScrollRef.clientWidth * (scrollLeft ? 1 : -1);
+                Math.ceil(currScrollRef.getBoundingClientRect().width + 64) *
+                (scrollLeft ? 1 : -1);
         }
     };
 
     return (
-        <div className="flex flex-row items-center my-8">
-            <button
-                className="hidden md:block mr-4 h-[40px] w-[40px]"
-                onClick={() => handleScroll(false)}
-            >
-                <IoIosArrowBack />
-            </button>
-            <div
-                style={{
-                    scrollBehavior: 'smooth'
-                }}
-                ref={scrollToRef}
-                className="snap-x flex flex-row overflow-x-scroll gap-x-4 items-center"
-            >
-                {images.map((image) => (
-                    <img
-                        className="snap-start md:max-w-[50%] max-h-[20%]"
-                        src={urlFor(image.image).width(800).url()}
-                        alt={image.altText}
-                    />
-                ))}
+        <>
+            <div className="flex flex-row items-center my-8">
+                <button
+                    className="hidden md:block mr-4 h-[40px] w-[40px]"
+                    onClick={() => handleScroll(false)}
+                >
+                    <IoIosArrowBack />
+                </button>
+                <div
+                    style={{
+                        scrollBehavior: 'smooth'
+                    }}
+                    ref={scrollToRef}
+                    className="snap-x snap-mandatory flex flex-row overflow-x-scroll items-center no-scrollbar"
+                >
+                    {images.map((image, index) => (
+                        <img
+                            key={'img-gallery-' + index}
+                            onClick={() => setChosenImg(image)}
+                            className="cursor-pointer snap-start md:max-w-[50%] max-h-[20%] mr-4 last-of-type:mr-0 "
+                            src={urlFor(image.image).width(800).url()}
+                            alt={image.altText}
+                        />
+                    ))}
+                </div>
+                <button
+                    className="hidden md:block ml-4 h-[40px] w-[40px]"
+                    onClick={() => handleScroll(true)}
+                >
+                    <IoIosArrowForward />
+                </button>
             </div>
-            <button
-                className="hidden md:block ml-4 h-[40px] w-[40px]"
-                onClick={() => handleScroll(true)}
-            >
-                <IoIosArrowForward />
-            </button>
-        </div>
+            {chosenImg && (
+                <ImageModal chosenImg={chosenImg} setChosenImg={setChosenImg} />
+            )}
+        </>
     );
 };
 
---
+++ src/components/ImageModal.tsx
@@ -0,0 +1,85 @@
+import { urlFor } from '../lib/sanity.ts';
+import { SanityImage } from '../lib/types.ts';
+import { useEffect, useRef, useState } from 'react';
+import { assertIsNode } from '../lib/helpers.ts';
+import LoadingSpinner from './LoadingSpinner.tsx';
+import { RxCross2 } from 'react-icons/rx';
+
+const ImageModal = ({
+    chosenImg,
+    setChosenImg
+}: {
+    chosenImg: SanityImage;
+    setChosenImg: (img: SanityImage | undefined) => void;
+}) => {
+    const refElement = useRef<HTMLImageElement | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            const { target } = event;
+            assertIsNode(target);
+            if (
+                refElement.current &&
+                !refElement.current.contains(target as Node)
+            ) {
+                setChosenImg(undefined);
+            }
+        };
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            // Cleanup: remove event listener on unmount
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [refElement, setChosenImg]);
+
+    useEffect(() => {
+        document.body.style.overflow = 'hidden';
+        return () => {
+            document.body.style.overflow = 'unset';
+        };
+    }, []);
+
+    return (
+        <div
+            className="fixed top-0 z-10 left-0 h-full w-full"
+            style={{
+                backgroundColor: 'rgba(0,0,0,0.8)'
+            }}
+        >
+            <div className="flex items-center justify-center h-full w-full gap-x-4 p-2 md:p-10">
+                <div
+                    ref={refElement}
+                    className="relative max-w-full max-h-full"
+                >
+                    {!isLoading && (
+                        <button
+                            className="absolute top-5 right-5 bg-black rounded-full p-2"
+                            onClick={() => setChosenImg(undefined)}
+                        >
+                            <RxCross2 color="white" />
+                        </button>
+                    )}
+                    <img
+                        loading={'lazy'}
+                        className="object-contain max-h-[90vh] max-w-[90vw]"
+                        src={urlFor(chosenImg.image).url()}
+                        onLoad={() => {
+                            setIsLoading(false);
+                        }}
+                    />
+                    {chosenImg.caption && (
+                        <p
+                            className="absolute bottom-5 left-0 right-0 mx-auto text-white m-0 max-w-[300px] p-2 md:max-w-[500px]"
+                            style={{ backgroundColor: 'rgba(0,0,0,0.8)' }}
+                        >
+                            {chosenImg.caption}
+                        </p>
+                    )}
+                </div>
+                {isLoading && <LoadingSpinner />}
+            </div>
+        </div>
+    );
+};
+export default ImageModal;
