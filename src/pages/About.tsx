cc74db7dd7faab8add06fd555602bed7cffda92c
Siddise Hirpa <siddisehirpa@gmail.com>
2024-10-16 18:08:05 +0200

    wip about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react';
+import { getAboutPage } from '../lib/sanity';
+import { AboutPage } from '../lib/types';
+import { PortableText } from '@portabletext/react';
+import PortableTextComponent from '../components/PortableTextComponent';
+
 const About = () => {
-    return <h1>About</h1>;
+    const [aboutPage, setAboutPage] = useState<AboutPage>();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        getAboutPage()
+            .then((response: AboutPage) => {
+                console.log(response);
+                setAboutPage(response);
+            })
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    return (
+        <main className="flex flex-col justify-center items-center my-16">
+            {!isLoading && aboutPage && (
+                <>
+                    <h1>{aboutPage.title}</h1>
+                    <div className="mx-0 my-auto w-3/4">
+                        <PortableText
+                            value={aboutPage.content}
+                            components={PortableTextComponent}
+                        />
+                    </div>
+                </>
+            )}
+        </main>
+    );
 };
 
 export default About;
