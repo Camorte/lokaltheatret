dec12f022b0862fd0ee17131fa7885097e773a03
Siddise Hirpa <siddisehirpa@gmail.com>
2024-10-16 19:20:06 +0200

    add schema for about area

--- lokaltheatret-cms/schemaTypes/about.tsx
+++ lokaltheatret-cms/schemaTypes/about.tsx
@@ -12,6 +12,36 @@ export const about = defineType({
       validation: (Rule) => Rule.required(),
     }),
     defineField({
+      name: 'bannerColor',
+      title: 'Farge bak tittel',
+      type: 'color',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      title: 'Banner bilde',
+      name: 'aboutPageBannerImg',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      validation: (Rule) => Rule.required(),
+      fields: [
+        {
+          title: 'Alt. text',
+          name: 'alt',
+          type: 'string',
+          validation: (rule) =>
+            rule.custom((value, context) => {
+              const parent = context?.parent as {asset?: {_ref?: string}}
+
+              return !value && parent?.asset?._ref
+                ? 'Alt text is required when an image is present'
+                : true
+            }),
+        },
+      ],
+    }),
+    defineField({
       title: 'Innhold',
       name: 'content',
       type: 'array',
