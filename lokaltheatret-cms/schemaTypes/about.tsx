43f5f9776690cf62448b249f1e6954235ef01456
Siddise Hirpa <siddisehirpa@gmail.com>
2024-10-16 20:41:19 +0200

    add founders

--- lokaltheatret-cms/schemaTypes/about.tsx
+++ lokaltheatret-cms/schemaTypes/about.tsx
@@ -4,6 +4,7 @@ export const about = defineType({
   title: 'Om oss',
   name: 'about',
   type: 'document',
+  fieldsets: [{name: 'founders', title: 'Våre grunnleggere'}],
   fields: [
     defineField({
       title: 'Tittel',
@@ -74,6 +75,62 @@ export const about = defineType({
         }),
       ],
       validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      title: 'Innhold',
+      name: 'foundersContent',
+      type: 'array',
+      fieldset: 'founders',
+      of: [
+        defineArrayMember({
+          type: 'block',
+        }),
+        defineArrayMember({
+          type: 'image',
+          fields: [
+            {
+              name: 'caption',
+              type: 'string',
+              title: 'Caption',
+            },
+            {
+              title: 'Alt. text',
+              name: 'alt',
+              type: 'string',
+              validation: (rule) =>
+                rule.custom((value, context) => {
+                  const parent = context?.parent as {asset?: {_ref?: string}}
+
+                  return !value && parent?.asset?._ref
+                    ? 'Alt text is required when an image is present'
+                    : true
+                }),
+            },
+          ],
+        }),
+      ],
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      title: 'Grunnleggere',
+      name: 'foundersList',
+      type: 'array',
+      fieldset: 'founders',
+      of: [
+        {
+          title: 'Grunnleggerne',
+          type: 'object',
+          fields: [
+            {
+              title: 'Navn',
+              name: 'name',
+              type: 'string',
+            },
+            {title: 'Rolle', name: 'role', type: 'string'},
+            {title: 'Bilde', name: 'image', type: 'image'},
+          ],
+        },
+      ],
     }),
   ],
 })
