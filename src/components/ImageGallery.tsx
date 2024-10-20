d9f9581b44d046a5b3d871f5e00af55f88093f25
Carlo Morte <carlo.alfredo.malca.miguel.morte@schibsted.com>
2024-10-20 12:46:41 +0200

    add button events on scroll and window resizing

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -2,7 +2,7 @@ import { SanityImage } from '../lib/types.ts';
 import { urlFor } from '../lib/sanity.ts';
 import { IoIosArrowForward } from 'react-icons/io';
 import { IoIosArrowBack } from 'react-icons/io';
-import { MutableRefObject, useRef, useState } from 'react';
+import { MutableRefObject, useEffect, useRef, useState } from 'react';
 import ImageModal from './ImageModal.tsx';
 
 const ImageGallery = ({ images }: { images: SanityImage[] }) => {
@@ -11,47 +11,102 @@ const ImageGallery = ({ images }: { images: SanityImage[] }) => {
         undefined
     );
 
+    const [maxScrollRight, setMaxScrollRight] = useState(true);
+    const [maxScrollLeft, setMaxScrollLeft] = useState(true);
+
+    const updateScrollMax = () => {
+        const currScrollRef = scrollToRef.current;
+        if (currScrollRef) {
+            setMaxScrollRight(currScrollRef.scrollLeft === 0);
+
+            setMaxScrollLeft(
+                currScrollRef.scrollLeft + currScrollRef.clientWidth >=
+                    currScrollRef.scrollWidth
+            );
+        }
+    };
+
+    const scrollOnFocus = () => {
+        if (document.activeElement) {
+            document.activeElement.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     const handleScroll = (scrollLeft: boolean) => {
         const currScrollRef = scrollToRef.current;
         if (currScrollRef) {
-            currScrollRef.scrollLeft +=
-                Math.ceil(currScrollRef.getBoundingClientRect().width + 64) *
-                (scrollLeft ? 1 : -1);
+            currScrollRef.scrollLeft += 700 * (scrollLeft ? 1 : -1);
         }
     };
+
+    useEffect(() => {
+        updateScrollMax();
+        const currScrollRef = scrollToRef.current;
+
+        if (currScrollRef) {
+            setMaxScrollLeft(
+                currScrollRef.scrollWidth <= currScrollRef.clientWidth
+            );
+        }
+    }, []);
+
+    useEffect(() => {
+        const currScrollRef = scrollToRef.current;
+
+        window.addEventListener('resize', updateScrollMax);
+
+        if (currScrollRef) {
+            currScrollRef.addEventListener('scroll', updateScrollMax);
+            currScrollRef.addEventListener('focus', scrollOnFocus);
+        }
+
+        return () => {
+            if (currScrollRef) {
+                currScrollRef.removeEventListener('scroll', updateScrollMax);
+                currScrollRef.removeEventListener('focus', scrollOnFocus);
+            }
+            window.removeEventListener('resize', updateScrollMax);
+        };
+    }, [scrollToRef]);
 
     return (
         <>
             <div className="flex flex-row items-center my-8">
-                <button
-                    className="hidden md:block mr-4 h-[40px] w-[40px]"
-                    onClick={() => handleScroll(false)}
-                >
-                    <IoIosArrowBack />
-                </button>
+                <div className="hidden md:block mr-4 h-[40px] w-[40px]">
+                    {!maxScrollRight && (
+                        <button onClick={() => handleScroll(false)}>
+                            <IoIosArrowBack />
+                        </button>
+                    )}
+                </div>
                 <div
                     style={{
                         scrollBehavior: 'smooth'
                     }}
                     ref={scrollToRef}
-                    className="snap-x snap-mandatory flex flex-row overflow-x-scroll items-center no-scrollbar"
+                    className="snap-x snap-mandatory flex flex-row overflow-x-scroll items-center no-scrollbar w-full"
                 >
                     {images.map((image, index) => (
-                        <img
+                        <div
                             key={'img-gallery-' + index}
                             onClick={() => setChosenImg(image)}
-                            className="cursor-pointer snap-start md:max-w-[50%] max-h-[15%] mr-4 last-of-type:mr-0 "
-                            src={urlFor(image.image).width(800).url()}
-                            alt={image.altText}
-                        />
+                            className="cursor-pointer shrink-0 snap-start h-full w-[350px] p-4"
+                        >
+                            <img
+                                className="w-full h-full"
+                                src={urlFor(image.image).width(450).url()}
+                                alt={image.altText}
+                            />
+                        </div>
                     ))}
                 </div>
-                <button
-                    className="hidden md:block ml-4 h-[40px] w-[40px]"
-                    onClick={() => handleScroll(true)}
-                >
-                    <IoIosArrowForward />
-                </button>
+                <div className="hidden md:block ml-4 h-[40px] w-[40px]">
+                    {!maxScrollLeft && (
+                        <button onClick={() => handleScroll(true)}>
+                            <IoIosArrowForward />
+                        </button>
+                    )}
+                </div>
             </div>
             {chosenImg && (
                 <ImageModal chosenImg={chosenImg} setChosenImg={setChosenImg} />
