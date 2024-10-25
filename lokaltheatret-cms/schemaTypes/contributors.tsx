67643e50f85d914795058c4809c5734676963e8d
Carlo Morte <carlo.alfredo.malca.miguel.morte@schibsted.com>
2024-10-25 16:26:53 +0200

    update contributors with optional image for actors

--- lokaltheatret-cms/schemaTypes/contributors.tsx
+++ lokaltheatret-cms/schemaTypes/contributors.tsx
@@ -1,22 +1,49 @@
-import {defineType} from 'sanity'
+import {defineArrayMember, defineField, defineType} from 'sanity'
 
 export const contributors = defineType({
   title: 'Medvirkende',
   name: 'contributors',
   type: 'object',
   fields: [
-    {
+    defineField({
       title: 'Skuespillere',
       name: 'actors',
       type: 'array',
       of: [
-        {
+        defineArrayMember({
           title: 'Skuespiller',
           name: 'actor',
           type: 'object',
           fields: [
-            {type: 'string', title: 'Rolle', name: 'role', validation: (Rule) => Rule.required()},
-            {
+            defineField({
+              type: 'string',
+              title: 'Rolle',
+              name: 'role',
+              validation: (Rule) => Rule.required(),
+            }),
+            defineField({
+              name: 'image',
+              type: 'image',
+              options: {
+                hotspot: true,
+              },
+              fields: [
+                {
+                  title: 'Alt. text',
+                  name: 'altText',
+                  type: 'string',
+                  validation: (rule) =>
+                    rule.custom((value, context) => {
+                      const parent = context?.parent as {asset?: {_ref?: string}}
+
+                      return !value && parent?.asset?._ref
+                        ? 'Alt text is required when an image is present'
+                        : true
+                    }),
+                },
+              ],
+            }),
+            defineField({
               title: 'Navn(ene)',
               name: 'names',
               type: 'array',
@@ -28,17 +55,17 @@ export const contributors = defineType({
                   validation: (Rule) => Rule.required(),
                 },
               ],
-            },
+            }),
           ],
-        },
+        }),
       ],
-    },
-    {
+    }),
+    defineField({
       title: 'Kunstnerisk lag',
       name: 'aristicTeam',
       type: 'array',
       of: [
-        {
+        defineArrayMember({
           title: 'Kunstner',
           name: 'artist',
           type: 'object',
@@ -58,15 +85,15 @@ export const contributors = defineType({
               ],
             },
           ],
-        },
+        }),
       ],
-    },
-    {
+    }),
+    defineField({
       title: 'Produksjonslag',
       name: 'productionTeam',
       type: 'array',
       of: [
-        {
+        defineArrayMember({
           title: 'Produsent',
           name: 'producer',
           type: 'object',
@@ -86,8 +113,8 @@ export const contributors = defineType({
               ],
             },
           ],
-        },
+        }),
       ],
-    },
+    }),
   ],
 })
