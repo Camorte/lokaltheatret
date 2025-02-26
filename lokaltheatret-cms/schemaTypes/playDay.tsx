9bef2091c114b5c89a6cbde75af42d151bafa4ed
Siddise Hirpa <siddisehirpa@gmail.com>
2025-02-26 21:07:32 +0100

    fix preview type error on playday

--- lokaltheatret-cms/schemaTypes/playDay.tsx
+++ lokaltheatret-cms/schemaTypes/playDay.tsx
@@ -4,6 +4,29 @@ export const playDay = defineType({
   title: 'Forestillingsdag',
   name: 'playDay',
   type: 'object',
+  preview: {
+    select: {
+      title: 'playDate',
+      soldOut: 'soldOut',
+      fewTickets: 'fewTickets',
+    },
+    prepare(selection) {
+      const {title, soldOut, fewTickets} = selection as {
+        title: string
+        soldOut: boolean
+        fewTickets: boolean
+      }
+      let subtitleText = soldOut ? 'Utsolgt' : 'Ikke utsolgt'
+      if (fewTickets) {
+        subtitleText += ' - Få billetter igjen'
+      }
+
+      return {
+        title: title,
+        subtitle: subtitleText,
+      }
+    },
+  },
   fields: [
     {
       title: 'Forestillingsdato',
@@ -20,22 +43,4 @@ export const playDay = defineType({
     {title: 'Utsolgt', name: 'soldOut', type: 'boolean', initialValue: false},
     {title: 'Få billetter', name: 'fewTickets', type: 'boolean', initialValue: false},
   ],
-  preview: {
-    select: {
-      title: 'playDate',
-      soldOut: 'soldOut',
-      fewTickets: 'fewTickets',
-    },
-    prepare({title, soldOut, fewTickets}: {title: string; soldOut: boolean; fewTickets: boolean}) {
-      let subtitleText = soldOut ? 'Utsolgt' : 'Ikke utsolgt'
-      if (fewTickets) {
-        subtitleText += ' - Få billetter igjen'
-      }
-
-      return {
-        title: title,
-        subtitle: subtitleText,
-      }
-    },
-  },
 })
