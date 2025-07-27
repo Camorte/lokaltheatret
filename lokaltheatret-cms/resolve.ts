925559b6b02c4e83d8894491a557922341b9d645
Carlo Morte <carlo.alfredo.malca.miguel.morte@schibsted.com>
2025-07-27 22:08:18 +0200

    update packages, set config for previewing and update gitignore

---
+++ lokaltheatret-cms/resolve.ts
@@ -0,0 +1,86 @@
+import {defineDocuments, defineLocations, type DocumentLocation} from 'sanity/presentation'
+
+// resolveHref() is a convenience function that resolves the URL
+// path for different document types and used in the presentation tool.
+function resolveHref(documentType?: string, slug?: string): string | undefined {
+  switch (documentType) {
+    case 'play':
+      return `/forestillinger/${slug}`
+    default:
+      console.warn('Invalid document type:', documentType)
+      return undefined
+  }
+}
+
+export const resolve = {
+  // The Main Document Resolver API provides a method of resolving a main document from a given route or route pattern. https://www.sanity.io/docs/presentation-resolver-api#57720a5678d9
+  mainDocuments: defineDocuments([
+    {
+      route: '/',
+      filter: `_type == "landingPage"`,
+    },
+    {
+      route: '/om-oss',
+      filter: `_type == "about"`,
+    },
+    {route: '/bli-med', filter: `_type == "join"`},
+    {
+      route: '/forestillinger/:slug',
+      filter: `_type == "play" && slug.current == $slug`,
+    },
+  ]),
+  // Locations Resolver API allows you to define where data is being used in your application. https://www.sanity.io/docs/presentation-resolver-api#8d8bca7bfcd7
+  locations: {
+    footer: defineLocations({
+      locations: [
+        {
+          title: 'Footer',
+          href: '/',
+        },
+      ],
+      message: 'Dette dokumentet brukes i bunnteksten på alle sider',
+      tone: 'positive',
+    }),
+    landingPage: defineLocations({
+      locations: [
+        {
+          title: 'Landigsside',
+          href: '/',
+        },
+      ],
+      message: 'Dette dokumentet brukes på forsiden',
+    }),
+    about: defineLocations({
+      locations: [
+        {
+          title: 'Om oss',
+          href: '/om-oss',
+        },
+      ],
+      message: 'Dette dokumentet brukes på Om oss-siden',
+    }),
+    join: defineLocations({
+      locations: [
+        {
+          title: 'Om oss',
+          href: '/bli-med',
+        },
+      ],
+      message: 'Dette dokumentet brukes på Bli med-siden',
+    }),
+    play: defineLocations({
+      select: {
+        name: 'playTitle',
+        slug: 'slug.current',
+      },
+      resolve: (doc) => ({
+        locations: [
+          {
+            title: doc?.name || 'Untitled',
+            href: resolveHref('play', doc?.slug)!,
+          },
+        ],
+      }),
+    }),
+  },
+}
